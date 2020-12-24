b1a13cd1e1552f57676806b698f444d805dc002e
Yahia Ewida <yahia.ewida@integrant.com>
2020-12-24 15:56:27 +0200

    Initial Commit

---
+++ Covid19Monitor/Covid19MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19MonitorApp.swift
+//  Covid19Monitor
+//
+//  Created by Yahia Ewida on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct Covid19MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
